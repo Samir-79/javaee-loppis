145c1752ec5bc1bfea3f88aed49149bab0829a06
Samir_79 <samirovsky79@gmail.com>
2022-01-16 14:37:52 +0100

    Fixed number if copies restriction and reviewed README-file

---
+++ demo/demo/graphqldemo-211217/src/main/java/com/example/graphqldemo211217/entity/Book.java
@@ -0,0 +1,65 @@
+package com.example.graphqldemo211217.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String isbn;
+    private int pageCount;
+    @ManyToOne
+    private Author author;
+
+    public Book() {}
+
+    public Book(String title, String isbn, int pageCount, Author author) {
+        this.title = title;
+        this.isbn = isbn;
+        this.pageCount = pageCount;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+}
